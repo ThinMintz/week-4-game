ffd5f081e868f712314b875087e3b9ac57b35b5b
ThinMintz <rachel.rodriguez05@gmail.com>
2017-10-04 19:41:31 -0500

    now the clicks work, working on wins and losses score board

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Generate random number at the start of the game
+console.log('Rachel');
+//number appears on the screen
+
+//crystal images are each assigned a random number between 1-12
+
+//userScore is set to zero
+
